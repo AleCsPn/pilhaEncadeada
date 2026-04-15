dfacc2b51d20d3755ff41c45b7a7a247b0f3ebb4
AleCsPn <alexandrecostapenhasilva@gmail.com>
2026-04-15 13:00:21 -0300

    Upload

---
+++ src/PilhaEncadeada.java
@@ -0,0 +1,75 @@
+import java.util.Iterator;
+
+public class PilhaEncadeada<T> implements Iterable{
+    private No<T> topo;
+    private int size;
+
+    public PilhaEncadeada() {
+        this.topo = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.topo == null;
+    }
+
+    public boolean push(T dado) {
+        No<T> novoNo = new No<>(dado);
+        novoNo.setProximo(this.topo);
+        this.topo = novoNo;
+        this.size++;
+        return true;
+
+    }
+
+    public T pop() {
+        if (!isEmpty()) {
+            T removido = this.topo.getDado();
+            this.topo = this.topo.getProximo();
+            this.size--;
+            return removido;
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder pilha = new StringBuilder("*** Topo ***\n");
+        No<T> noAtual = this.topo;
+        while (noAtual != null) {
+            pilha.append(noAtual.getDado() + "\n");
+            noAtual = noAtual.getProximo();
+        }
+
+        pilha.append("__________\n");
+        return pilha.toString();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new PilhaIterator();
+    }
+
+    private class PilhaIterator implements Iterator<T> {
+        private No<T> atual;
+
+       public PilhaIterator(){
+            this.atual =topo;
+       }
+
+       @Override
+       public boolean hasNext(){
+        return atual != null;
+       }
+
+       @Override
+       public T next(){
+        if(!hasNext()){
+            return null;
+        }
+        T dado = atual.getDado(); //guardar valor do no atual
+        atual = atual.getProximo(); //move o ponteiro para o proximo
+        return dado;
+       }
+    }
+}
